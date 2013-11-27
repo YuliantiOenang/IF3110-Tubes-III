1e1c9c328ff00d03f6af975980260f8857860b23
IgnisSolus <aldy_wirawan@live.com>
2013-11-27 17:29:58 +0700

    login sudah bisa

--- WebContent/login.js
+++ WebContent/login.js
@@ -2,6 +2,7 @@ function verLogin(){
 				var username = document.forms['login']['username'].value;
 				var password = document.forms['login']['password'].value;
 				var err_login = document.getElementById("err_login");
+				var data = "username="+username+"&password="+password;
 				var isLogin = false;
 				if(window.XMLHttpRequest){
 					xmlhttp = new XMLHttpRequest();
@@ -13,11 +14,13 @@ function verLogin(){
 					if(xmlhttp.readyState==4&&xmlhttp.status==200){
 						err_login.innerHTML = xmlhttp.responseText;
 
-						if(err_login==""){
+						if(err_login.innerHTML==""){
 							//Handle SESSION & LOCAL STORAGE
+							window.location="index.jsp";
 						}
 					}
 				};
 				xmlhttp.open("POST","verifyLogin",true);
+				xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 				xmlhttp.send(data);
 			}
