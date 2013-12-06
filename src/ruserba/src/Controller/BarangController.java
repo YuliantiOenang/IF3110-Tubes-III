5800381a90ea450c1c5c501b33d6e01daea040da
darkstallion <darkstallion@bitbucket.org>
2013-12-06 22:07:34 +0700

    Barang Controller beres

--- src/ruserba/src/Controller/BarangController.java
+++ src/ruserba/src/Controller/BarangController.java
@@ -1,9 +1,12 @@
 package Controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import javaModel.Account;
 import javaModel.Barang;
 
 import javax.servlet.ServletException;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -47,13 +51,15 @@ public class BarangController extends HttpServlet {
     	            out.println("{\"status\":\"false\"}");
     	        }
     	    } else if (action.equals("readAll")) {
-    	        String username = request.getParameter("username");
-    	        String password = request.getParameter("password");
-                Account user = Account.find("SELECT * FROM account WHERE username = '" + username + "' AND password = '" + password + "'");
-                if (user != null) {
-                    JSONObject json = new JSONObject();
-                    json.put("status", "true");
-                    json.put("data", user.toJSON());
+    	        String id_kategori = request.getParameter("id_kategori");
+                ArrayList<Barang> barangs = Barang.findAll("SELECT * FROM barang WHERE id_kategori = '" + id_kategori + "'");
+                if (barangs.size() > 0) {
+                	ArrayList<JSONObject> jsons = new ArrayList<JSONObject>();
+					for (Barang barang : barangs)
+						jsons.add(barang.toJSON());
+					JSONObject json = new JSONObject();
+					json.put("status", "true");
+					json.put("data", new JSONArray(jsons));
                     out.println(json.toString());
                 } else {
                     out.println("{\"status\":\"false\"}");
@@ -72,4 +78,69 @@ public class BarangController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	}
 
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        BufferedReader in = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String input = in.readLine();
+        String[] datas = input.split("&");
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (String data : datas)
+        {
+        	System.out.println(data);
+        	String[] tuple = data.split("=");
+        	map.put(tuple[0], tuple[1]);
+        }
+        Integer id = Integer.parseInt(map.get("id"));
+        
+        Barang barang = Barang.findByPk(id);
+        try {
+            if (barang != null) {
+                //parameter atribut account
+                barang.id_kategori = Integer.parseInt(map.get("id_kategori"));
+                barang.nama = map.get("nama");
+                barang.harga = Integer.parseInt(map.get("harga"));
+                barang.gambar = map.get("gambar");
+                barang.stok = Integer.parseInt(map.get("stok"));
+                barang.counter = Integer.parseInt(map.get("counter"));
+                barang.keterangan = map.get("keterangan");
+                barang.save();
+                JSONObject json = new JSONObject();
+                json.put("status", "true");
+                out.println(json.toString());
+            } else {
+                out.println("{\"status\":\"false\"}");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        out.close();
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        BufferedReader in = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String[] tuple = in.readLine().split("=");
+        int id = Integer.parseInt(tuple[1]);
+        
+        Barang barang = Barang.findByPk(id);
+        try{
+        	if (barang != null)
+        	{
+        		barang.delete();
+        		JSONObject json = new JSONObject();
+        		json.put("status","true");
+        		out.println(json.toString());
+        	}
+        	else
+        		out.println("{\"status\":\"false\"}");
+        }
+        catch (Exception e){
+        	e.printStackTrace();
+        }
+        out.close();
+	}
 }
