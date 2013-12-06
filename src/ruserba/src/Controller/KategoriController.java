8ed11f94b5067e732b0441d860cc0cf1731963fb
darkstallion <darkstallion@bitbucket.org>
2013-12-06 22:35:53 +0700

    finish rest kategori

--- src/ruserba/src/Controller/KategoriController.java
+++ src/ruserba/src/Controller/KategoriController.java
@@ -1,11 +1,18 @@
 package Controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javaModel.Kategori;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class KategoriController
@@ -19,21 +26,51 @@ public class KategoriController extends HttpServlet {
      */
     public KategoriController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+	    String action = request.getParameter("action");
+	    try {
+    	    if (action.equals("read")) {
+    	        Integer id = Integer.parseInt(request.getParameter("id"));
+    	        Kategori kategori = Kategori.findByPk(id);
+    	        if (kategori != null) {
+    	            JSONObject json = new JSONObject();
+    	            json.put("status", "true");
+    	            json.put("data", kategori.toJSON());
+    	            out.println(json.toString());
+    	        } else {
+    	            out.println("{\"status\":\"false\"}");
+    	        }
+    	    } else if (action.equals("readAll")) {
+                ArrayList<Kategori> kategoris = Kategori.findAll("SELECT * FROM kategori");
+                if (kategoris.size() > 0) {
+                	ArrayList<JSONObject> jsons = new ArrayList<JSONObject>();
+					for (Kategori kategori : kategoris)
+						jsons.add(kategori.toJSON());
+					JSONObject json = new JSONObject();
+					json.put("status", "true");
+					json.put("data", new JSONArray(jsons));
+                    out.println(json.toString());
+                } else {
+                    out.println("{\"status\":\"false\"}");
+                }
+    	    }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		out.close();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
--- src/ruserba/src/Controller/KreditController.java
+++ src/ruserba/src/Controller/KreditController.java
@@ -19,21 +19,18 @@ public class KreditController extends HttpServlet {
      */
     public KreditController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
