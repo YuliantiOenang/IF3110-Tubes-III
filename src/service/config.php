4a052688f36442da87a51bca926827495ed8fa7d
Faiz Ilham <indigo.republica@gmail.com>
2013-12-06 15:50:27 +0700

    db configuration

--- src/service/config.php
+++ src/service/config.php
@@ -5,13 +5,34 @@
 /** Database Parameter **/
 
 function db_parameter(){
-	$param["host"] = "localhost"; // to be changed
-	$param["name"] = "wbd3";
+	$RELEASE = false; $param = array();
 	
-	$param["user"] = "root"; // to be changed
-	$param["pass"] = "root"; // to be changed
-	
+	if ($RELEASE){
+		$env = json_decode(getenv("VCAP_SERVICES"), true);
+		$mysqls = $env["mysql-5.1"];
+		
+		foreach($mysqls as $mysql){
+			if ($mysql["name"] == "rukoonline"){
+				$param["host"] = $mysql["credentials"]["host"];
+				$param["name"] = $mysql["credentials"]["name"];
+				$param["user"] = $mysql["credentials"]["username"];
+				$param["pass"] = $mysql["credentials"]["password"];
+				break;
+			}
+		}
+		
+	}else{
+		$param["host"] = "localhost"; // to be changed
+		$param["name"] = "wbd3";
+		$param["user"] = "root"; // to be changed
+		$param["pass"] = "root"; // to be changed
+	}
 	return $param;
+}
+
+function db_connect(){
+	$dbparam = db_parameter();
+	return new mysqli($dbparam["host"], $dbparam["user"], $dbparam["pass"], $dbparam["name"]);
 }
 
 ?>
