bfc1ef43d96882b718a1cecb76674cea1ae284f8
Setyo Legowo <gw.tio145@gmail.com>
2013-12-07 16:30:39 +0700

    coba

---
+++ src/thelastruserba/src/userController/ProfileController.java
@@ -0,0 +1,68 @@
+package userController;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javaModel.Helper;
+import javaModel.Profile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import databaseLib.DatabaseAdapter;
+
+public class ProfileController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	DatabaseAdapter DBA = new DatabaseAdapter();
+    
+    public ProfileController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+//		if (request.getParameter("ignore")==null)
+//			if (!Helper.isCreditCard(request.getSession())) {
+//				response.sendRedirect("/ruserba/profile/credit");
+//				return;
+//			}
+//		String username = Helper.getUserLogged(request.getSession());
+//		if (username.isEmpty()) {
+//			response.sendRedirect("/ruserba/register");
+//			return;
+//		}
+		String username = (String) request.getAttribute("username");
+		String q = "select * from account where username = '" + username  + "' limit 1";
+		Profile P = new Profile(DBA);
+		P.executeQuery(q);
+		String result = null;
+		try {
+			JSONObject json = new JSONObject().put("nam_lengkap", P.nama.get(0));
+			json.put("username", P.username.get(0));
+			json.put("email", P.email.get(0));
+			json.put("provinsi", P.provinsi.get(0));
+			json.put("kota", P.kota.get(0));
+			json.put("no_telp", P.telepon.get(0));
+			json.put("alamat", P.alamat.get(0));
+			json.put("kode_pos", P.kodepos.get(0));
+			json.put("transaksi", P.transaksi.get(0));
+			result = json.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		out.print(result);
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+}
