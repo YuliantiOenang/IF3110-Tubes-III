14e767abe1e78c2f51e3c00a26279b45844c4330
Lubis Sucipto <loe_biez@ymail.com>
2013-12-07 15:17:32 +0700

    set cookie cart

--- src/ruserbaclient/js/shop.js
+++ src/ruserbaclient/js/shop.js
@@ -28,13 +28,12 @@ function beliBarang()
 {
 	xmlhttpShop.open("GET","/ruserba/barang/beli?id_barang="+id_barang+"&jumlah="+jumlah+"&keterangan="+keterangan,true);
 	xmlhttpShop.send();
-	jumlah = "";
+	jumlah = getCookie(id_barang);
 	keterangan = "";
 }
 
 function pertanyaan(id_b,stok)
 {
-	id_barang = id_b;
 	jumlah = document.getElementById("quantity_"+id_b).value;
 	keterangan = document.getElementById("keterangan_"+id_b).value;
 
@@ -42,8 +41,43 @@ function pertanyaan(id_b,stok)
 		alert("jumlah barang harus diisi");
 	else if (jumlah > stok) alert("Pemesanan barang melebihi stok, hanya tersisa "+stok+" barang");
 	else
-	{
-		beliBarang();
+	{	
+		setCookie(id_b,stok,30);
+		//beliBarang();
+		alert("cookie ok");
 	}
 	
+}
+
+function setCookie(c_name,value,exdays)
+{
+var exdate=new Date();
+exdate.setDate(exdate.getDate() + exdays);
+var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+var c_value = document.cookie;
+var c_start = c_value.indexOf(" " + c_name + "=");
+if (c_start == -1)
+  {
+  c_start = c_value.indexOf(c_name + "=");
+  }
+if (c_start == -1)
+  {
+  c_value = null;
+  }
+else
+  {
+  c_start = c_value.indexOf("=", c_start) + 1;
+  var c_end = c_value.indexOf(";", c_start);
+  if (c_end == -1)
+  {
+c_end = c_value.length;
+}
+c_value = unescape(c_value.substring(c_start,c_end));
+}
+return c_value;
 }
