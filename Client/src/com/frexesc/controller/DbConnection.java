c9b76ddd119162b54adf5193a5a8f65683fe1bab
reximir <davidlibraboy@gmail.com>
2013-12-08 06:31:12 +0700

    add sql connection localhost for debug

--- Client/src/com/frexesc/controller/DbConnection.java
+++ Client/src/com/frexesc/controller/DbConnection.java
@@ -6,10 +6,17 @@ public class DbConnection {
 	Connection connection = null;
 
 	public Connection mySqlConnection() {
-		String url = "jdbc:mysql://10.0.28.130:3306/d49533220d59549ea8d59a42dd0c22251";
-		String driver = "com.mysql.jdbc.Driver";
-		String username = "uq4KXgZdPVKL4";
-		String password = "pUxhgVj9EKMdX";
+		/**AppFog Setting*/
+//		String url = "jdbc:mysql://10.0.28.130:3306/d49533220d59549ea8d59a42dd0c22251";
+//		String driver = "com.mysql.jdbc.Driver";
+//		String username = "uq4KXgZdPVKL4";
+//		String password = "pUxhgVj9EKMdX";
+
+		/**Localhost setting*/
+		 String url = "jdbc:mysql://localhost:3306/frexesc";
+         String driver = "com.mysql.jdbc.Driver";
+         String username = "root";
+         String password = "";
 
 		try {
 			Class.forName(driver).newInstance();
