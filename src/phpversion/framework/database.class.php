6430ac58e38a861c3d2968236db0b9c85006368d
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-12-08 14:48:50 +0700

    Add Heroku connection

--- src/phpversion/framework/database.class.php
+++ src/phpversion/framework/database.class.php
@@ -25,6 +25,20 @@ class Database {
 	*
 	*/
 	public static function getInstance($registry) {
+		/* HEROKU CONNECT
+		# This function reads your DATABASE_URL configuration automatically set by Heroku
+		# the return value is a string that will work with pg_connect
+		function pg_connection_string() {
+		  return "dbname=abcdefg host=****.amazonaws.com port=5432 user=**** password=**** sslmode=require";
+		}
+		 
+		# Establish db connection
+		$db = pg_connect(pg_connection_string());
+		if (!$db) {
+		   echo "Database connection error."
+		   exit;
+		}
+		*/
 		if (!self::$instance) {
 			try {
 				$host = $registry->config['mysql']['host'];
