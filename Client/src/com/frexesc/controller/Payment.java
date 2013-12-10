a8a20546e57b08f1b47f648df2b1b42e2e84e059
Vai <vai03@yahoo.com>
2013-12-10 19:08:13 +0700

    Payment.java added barang_user web service.

--- Client/src/com/frexesc/controller/Payment.java
+++ Client/src/com/frexesc/controller/Payment.java
@@ -88,11 +88,16 @@ public class Payment extends HttpServlet {
 						response.sendRedirect("../card?from=payment");
 					} else {
 
-						// Update status
-						String query2 = "UPDATE barang_user SET status=1 WHERE id_user="
-								+ session.getAttribute("user_id");
-						connection.createStatement().executeUpdate(query2);
+												// Update status
+//						String query2 = "UPDATE barang_user SET status=1 WHERE id_user="
+//								+ session.getAttribute("user_id");
+//						connection.createStatement().executeUpdate(query2);
 
+						//POST
+						String[] param = {"id"};
+						String[] val= {"" + session.getAttribute("user_id")};
+						ServiceParser.postUrl(ServiceParser.BASE_URL + "BarangUserService/baranguserService/statusbaranguser",param, val);
+						
 						/**port*/
 
 							json = webkit.readUrl("http://localhost:8080/web-services/UserService/userservice/user/"+ session.getAttribute("user_id"));
@@ -149,13 +154,6 @@ public class Payment extends HttpServlet {
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 
