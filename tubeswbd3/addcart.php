78a8639343b72523a2c25e062e6a111c1adfd57a
elvenunion <adhikaaryantio.x6@gmail.com>
2013-12-11 16:13:17 +0700

    Delete Clean

--- tubeswbd3/addcart.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['shopping_cart'][$_POST['id_barang']] = $_POST['qt'];
-	$_SESSION['shopping_request'][$_POST['id_barang']] = $_POST['request_tambahan'];
-	header('location:index.php');
-?>
--- tubeswbd3/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include 'connect.php';
-	include 'header.php';
-	include 'ListBarang.php';
-	include 'footer.php';
-	?>
