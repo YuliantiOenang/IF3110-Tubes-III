361c53beb967f0dcd76a506f234c9d7c20c984c2
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-12-17 11:18:06 +0700

    Finish SOAP server test

---
+++ src/phpversion/server/app/controllers/server.php
@@ -0,0 +1,23 @@
+<?php
+
+Class ServerController extends BaseController {
+
+	public function index() {
+		if(!extension_loaded("soap")){
+		  dl("php_soap.dll");
+		}
+		 
+		ini_set("soap.wsdl_cache_enabled","0");
+		$location = "http:" . SITEURL . "/wsdl/hello.wsdl";
+		$server = new SoapServer($location);
+		 
+		function doHello($yourName){
+		  return "Hello, ".$yourName;
+		}
+		 
+		$server->AddFunction("doHello");
+		$server->handle();
+
+
+	}
+}
